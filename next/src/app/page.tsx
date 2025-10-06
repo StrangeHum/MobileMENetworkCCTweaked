479e6047e68acd20e17ae08af3b129cec0825dcc
strangehoney <vadim.k.2004@yandex.ru>
2025-10-06 12:55:52 +0300

    Создан хук для получения хранилища

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -2,44 +2,16 @@
 
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
-
-const apis = {
-  update: "http://192.168.0.105:8081/update",
-};
-
-type item = {
-  id: string;
-  count: number;
-  craftable: boolean;
-};
+import { useUpdateStoreage } from "./storage/storageSystem";
 
 export default function Home() {
-  const [items, setItems] = useState<item[]>([]);
-
-  useEffect(() => {
-    fetch(apis.update, {
-      method: "GET", // или "GET", если сервер это поддерживает
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log("Получены данные:", data);
-        setItems(data);
-      })
-      .catch((err) => console.error("Ошибка при загрузке:", err));
-  }, []);
-
-  // useEffect(() => {
-  //   setItems([{ id: "gtceu:lapis_dust", count: 140, craftable: false }]);
-  // }, []);
+  const storage = useUpdateStoreage();
 
   return (
     <div className={styles.page}>
       <h1>Инвентарь</h1>
       <ul>
-        {items.map((item) => (
+        {storage.map((item) => (
           <li key={item.id}>
             <strong>{item.id}</strong> — {item.count} шт.{" "}
             {item.craftable ? "(создаётся)" : ""}
--- next/src/app/storage/storageSystem.ts
+++ next/src/app/storage/storageSystem.ts
@@ -1,21 +1,35 @@
 import { get } from "http";
+import { useEffect, useState } from "react";
+type item = {
+  id: string;
+  count: number;
+  craftable: boolean;
+};
 
 const apis = {
   update: "http://192.168.0.105:8081/update",
 };
 
-export const foo = new Promise((resolve, reject) => {
+export const updateStorage = (callback: any) => {
   fetch(apis.update, {
-    method: "POST",
+    method: "GET", // или "GET", если сервер это поддерживает
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({}),
   })
-    .then((response) => {
-      reject(response);
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Получены данные:", data);
+      callback(data);
     })
-    .catch((error) => {
-      console.error(error);
-    });
-});
+    .catch((err) => console.error("Ошибка при загрузке:", err));
+};
+
+export const useUpdateStoreage = () => {
+  const [items, setItems] = useState<item[]>([]);
+
+  useEffect(() => {
+    updateStorage(setItems);
+  }, []);
+  return items;
+};
